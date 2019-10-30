d0fd7dff0da2312c3e3e0e09bfe87adf104f0e41
Andrew Lund <andrew.s.lund@gmail.com>
2019-10-30 11:47:17 -0700

    First commit

---
+++ src/renamefile.py
@@ -0,0 +1,44 @@
+from datetime import datetime, date
+from pytz import timezone
+import os
+import re
+import sys
+
+class RenameFile:
+	'''Class for renaming files downloaded from Ekos in preperation for
+	reformatting data using pandas and then importing data into Google
+	Sheets'''
+
+	def dtround(self, datetime):
+		'''Rounds seconds of datetime object up if microseconds > 500000
+		else returns datetime object'''
+		print("Rounding %s" % str(datetime))
+		if datetime.microsecond > 500000 and datetime.second == 59:
+			datetime = datetime.replace(minute = datetime.minute + 1, 
+				second = 0)
+		elif datetime.microsecond > 500000:
+			datetime = datetime.replace(second = datetime.second + 1)
+		return datetime
+
+	def tzconv(self, datetime):
+		'''Takes datetime object, sets timezone as Pacific Time
+		and the convents timezone to Eastern Time. Necessary because
+		Ekos export names incorporate time of download in Eastern Time.
+		DST = Daylight Savings Time'''
+		print("Adjusting Timezones")
+		datetime = timezone('UTC').localize(datetime)
+		datetime = datetime.astimezone(timezone('US/Eastern'))
+		return datetime
+
+	def rename_file(self, new_filename, PATH):
+		'''Searches for the downloaded csv file based on a regular expression
+		and replaces that filename with a filename provided '''
+		filename_regex = re.compile('Export_\d\d\d\d\d\d\d\d\d\d\d\d\d\d_\.csv')
+		for l in os.listdir(PATH):
+			if filename_regex.match(l) != None:
+				filename = filename_regex.match(l).string
+				os.rename(PATH + filename, PATH + new_filename)
+			else:
+				logger.info('No file matching regular expression')
+		return
+	
---
+++ src/sendemail.py
@@ -0,0 +1,128 @@
+import os
+import smtplib
+import mimetypes
+import logging
+from email.MIMEMultipart import MIMEMultipart
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+
+# Set up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+# Create file handler
+fh = logging.FileHandler('DeliveryFormat/deliveryformat.log') # PATH to file on local machine
+fh.setLevel(logging.INFO)
+# Create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Add formatter to fh
+fh.setFormatter(formatter)
+# Add fh to logger
+logger.addHandler(fh)
+
+class SendEmail:
+	# Class for sending emails from gmail account
+
+	def send_email(self, message, subject, emailTo, emailFrom, password, fileToSend=None):
+		# Sends email with provided file as attachment from emailFrom to emailTo
+		# Username and Password provided for gmail acount that sends email
+
+		logger.info('Sending Email')
+
+		msg = MIMEMultipart()
+		msg['From'] = emailFrom
+		if type(emailTo) == list:
+			msg['To'] = ', '.join(emailTo)
+		else:
+			msg['To'] = emailTo
+		msg['Reply-To'] = 'NoReplay@NoReply.com'
+		msg['Subject'] = subject
+
+		body = MIMEText(message)
+		msg.attach(body)
+
+		if fileToSend == None:
+			pass
+		elif type(fileToSend) == list:	# Allows for multiple attachments
+			for f in fileToSend:
+				
+				ctype, encoding = mimetypes.guess_type(f)
+				if ctype is None or encoding is not None:
+					ctype = 'application/octet-stream'
+
+				maintype, subtype = ctype.split('/', 1)
+
+				if maintype == 'application':
+					fp = open(f, 'rb')
+					att = MIMEApplication(fp.read(), _subtype=subtype)
+					fp.close()
+				elif maintype == 'text':
+					fp = open(f)
+					att = MIMEText(fp.read(), _subtype=subtype)
+					fp.close()
+				elif maintype == 'image':
+					fp = open(f, 'rb')
+					att = MIMEImage(fp.read(), _subtype=subtype)
+					fp.close()
+				elif maintype == 'audio':
+					fp = open(f, 'rb')
+					att = MIMEAudio(fp.read(), _subtype=subtype)
+					fp.close()
+				else:
+					fp = open(f, 'rb')
+					att = MIMEBase(maintype, subtype)
+					att.set_payload(fp.read())
+					fp.close()
+					encoders.encode_base64(att)
+
+				att.add_header('content-disposition', 'attachment', filename=os.path.basename(f))
+				msg.attach(att)
+		else:
+			ctype, encoding = mimetypes.guess_type(fileToSend)
+			if ctype is None or encoding is not None:
+				ctype = 'application/octet-stream'
+
+			maintype, subtype = ctype.split('/', 1)
+
+			if maintype == 'application':
+					fp = open(fileToSend, 'rb')
+					att = MIMEApplication(fp.read(), _subtype=subtype)
+					fp.close()
+			elif maintype == 'text':
+				fp = open(fileToSend)
+				att = MIMEText(fp.read(), _subtype=subtype)
+				fp.close()
+			elif maintype == 'image':
+				fp = open(fileToSend, 'rb')
+				att = MIMEImage(fp.read(), _subtype=subtype)
+				fp.close()
+			elif maintype == 'audio':
+				fp = open(fileToSend, 'rb')
+				att = MIMEAudio(fp.read(), _subtype=subtype)
+				fp.close()
+			else:
+				fp = open(fileToSend, 'rb')
+				att = MIMEBase(maintype, subtype)
+				att.set_payload(fp.read())
+				fp.close()
+				encoders.encode_base64(att)
+
+			att.add_header('content-disposition', 'attachment', filename=os.path.basename(fileToSend))
+			msg.attach(att)
+
+		server = smtplib.SMTP('smtp.gmail.com', 587)
+		server.ehlo()
+		server.starttls()
+		server.ehlo()
+		server.login(emailFrom, password)
+		server.sendmail(emailFrom, emailTo, msg.as_string())
+		server.quit()
+
+		return
+
+
+
+
