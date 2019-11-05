f614397f5d80d6d57d327a9b56bc3e980175adb5
Andrew Lund <andrew.s.lund@gmail.com>
2019-11-05 12:49:58 -0800

    fixed inconsistent indents

--- src/datareformat.py
+++ src/datareformat.py
@@ -18,105 +18,105 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 
 try:
-	class DataReformat:
-		# Class for reformatting Empty Keg and Outstanding Balance data from Ekos
+    class DataReformat:
+        # Class for reformatting Empty Keg and Outstanding Balance data from Ekos
 
-		def data_reformat_empties(self, PATH, filename):
-			# Reformats data from "Kegs At Customers - Pivoted by Keg 
-			# Type/Quantity [Script Report]" report from Ekos
-			
-			logger.info('Reformatting %s data' % filename)
+        def data_reformat_empties(self, PATH, filename):
+            # Reformats data from "Kegs At Customers - Pivoted by Keg 
+            # Type/Quantity [Script Report]" report from Ekos
+            
+            logger.info('Reformatting %s data' % filename)
 
-			# Import and read csv file located in PATH
-			logger.debug('Importing file %s' % filename)
-			targetFile = PATH + filename
-			df = pd.read_csv(targetFile)
+            # Import and read csv file located in PATH
+            logger.debug('Importing file %s' % filename)
+            targetFile = PATH + filename
+            df = pd.read_csv(targetFile)
 
-			# Convert Most Recent Delivery to datetime objects
-			# First drop NaNs in Most Recent Delivery column
-			df = df[df['Most Recent Delivery'].isna() == False]
-			# Convert Most Recent Delivery column to datetimes
-			df['Most Recent Delivery'] = pd.to_datetime(df['Most Recent Delivery'])
+            # Convert Most Recent Delivery to datetime objects
+            # First drop NaNs in Most Recent Delivery column
+            df = df[df['Most Recent Delivery'].isna() == False]
+            # Convert Most Recent Delivery column to datetimes
+            df['Most Recent Delivery'] = pd.to_datetime(df['Most Recent Delivery'])
 
-			# Limit date range in data frame - 30 days ago to 6 months ago
-			logger.debug('Limiting Delivery Date Range')
-			# Ditch dates after 30 days before today
-			last_month = pd.datetime.today() - pd.Timedelta(days=30)
-			df = df[df['Most Recent Delivery'] <= last_month]
-			# Ditch accounts that haven't ordered in the last 6 months
-			six_months_ago = pd.datetime.today() - pd.Timedelta(days=180)
-			df = df[df['Most Recent Delivery'] >= six_months_ago]
+            # Limit date range in data frame - 30 days ago to 6 months ago
+            logger.debug('Limiting Delivery Date Range')
+            # Ditch dates after 30 days before today
+            last_month = pd.datetime.today() - pd.Timedelta(days=30)
+            df = df[df['Most Recent Delivery'] <= last_month]
+            # Ditch accounts that haven't ordered in the last 6 months
+            six_months_ago = pd.datetime.today() - pd.Timedelta(days=180)
+            df = df[df['Most Recent Delivery'] >= six_months_ago]
 
-			# Fill NaNs
-			logger.debug('Filling NaNs')
-			# Quantity Columns with 0s
-			df[['Keg Shell - 13.2 Gal', 'Keg Shell - Sixtel']] = df[['Keg Shell - 13.2 Gal', 'Keg Shell - Sixtel']].fillna(0)
-			#df.info()
-			# Salesperson Column with None
-			df['Salesperson'] = df['Salesperson'].fillna('None')
+            # Fill NaNs
+            logger.debug('Filling NaNs')
+            # Quantity Columns with 0s
+            df[['Keg Shell - 13.2 Gal', 'Keg Shell - Sixtel']] = df[['Keg Shell - 13.2 Gal', 'Keg Shell - Sixtel']].fillna(0)
+            #df.info()
+            # Salesperson Column with None
+            df['Salesperson'] = df['Salesperson'].fillna('None')
 
-			# Split data by Sales Rep and write to csvs
-			logger.debug('Writing updated %s to Sales Rep csvs' % filename)
-			sales_reps = df['Salesperson'].unique()
-			for rep in sales_reps:
-				df[df['Salesperson'] == rep].to_csv(path_or_buf=PATH+'%s_Empties.csv' % rep.replace(' ', '_'), 
-			                                        columns=df.columns, 
-			                                        index=False)
-			return
+            # Split data by Sales Rep and write to csvs
+            logger.debug('Writing updated %s to Sales Rep csvs' % filename)
+            sales_reps = df['Salesperson'].unique()
+            for rep in sales_reps:
+                df[df['Salesperson'] == rep].to_csv(path_or_buf=PATH+'%s_Empties.csv' % rep.replace(' ', '_'), 
+                                                    columns=df.columns, 
+                                                    index=False)
+            return
 
-		def data_reformat_overdue(self, PATH, filename):
-			# Reformats data from "Invoice - Overdue Balances Summed by Company" report
+        def data_reformat_overdue(self, PATH, filename):
+            # Reformats data from "Invoice - Overdue Balances Summed by Company" report
 
-			logger.info('Reformatting %s data' % filename)
+            logger.info('Reformatting %s data' % filename)
 
-			#Import and read csv file located in PATH
-			logger.debug('Importing file %s' % filename)
-			targetFile = PATH + filename
-			df = pd.read_csv(targetFile)
-			
-			# Fill Salesperson NaNs with None
-			logger.debug('Filling Salesperson NaNs')
-			df['Salesperson'] = df['Salesperson'].fillna('None')
+            #Import and read csv file located in PATH
+            logger.debug('Importing file %s' % filename)
+            targetFile = PATH + filename
+            df = pd.read_csv(targetFile)
+            
+            # Fill Salesperson NaNs with None
+            logger.debug('Filling Salesperson NaNs')
+            df['Salesperson'] = df['Salesperson'].fillna('None')
 
-			# Convert Due Date Column to datetime objects
-			logger.debug('Converting Due Date Column to datetime objects')
-			df['Due Date'] = pd.to_datetime(df['Due Date'])
+            # Convert Due Date Column to datetime objects
+            logger.debug('Converting Due Date Column to datetime objects')
+            df['Due Date'] = pd.to_datetime(df['Due Date'])
 
-			# Add conditional Status column
-			# "Credit Hold Pending" for due dates passed 30 days due
-			# "Newly Overdue" for due dates last week
-			# "Overdue" for all others
-			logger.debug('Creating Status Column')
-			df['Balance Status'] = ['Credit Hold Pending' if x < (pd.datetime.today() - pd.Timedelta(days= 30)) 
+            # Add conditional Status column
+            # "Credit Hold Pending" for due dates passed 30 days due
+            # "Newly Overdue" for due dates last week
+            # "Overdue" for all others
+            logger.debug('Creating Status Column')
+            df['Balance Status'] = ['Credit Hold Pending' if x < (pd.datetime.today() - pd.Timedelta(days= 30)) 
                         else 'Newly Overdue' if x > (pd.datetime.today() - pd.Timedelta(days=7))
                         else 'Overdue' for x in df['Due Date']]
             # Change Balance Status Column to reflect current credit holds and inactive accts
             logger.debug('Updating Status Column')
-			df.loc[df['Ekos Status'] == 'Credit Hold', 'Balance Status'] = 'Credit Hold'
-			df.loc[df['Ekos Status'] == 'Inactive', 'Balance Status'] = 'Inactive'
+            df.loc[df['Ekos Status'] == 'Credit Hold', 'Balance Status'] = 'Credit Hold'
+            df.loc[df['Ekos Status'] == 'Inactive', 'Balance Status'] = 'Inactive'
 
-			# Split data by Sales Rep and write to csvs
-			logger.debug('Writing updated %s to Sales Rep csvs' % filename)
-			sales_reps = df['Salesperson'].unique()
-			for rep in sales_reps:
-				df[df['Salesperson'] == rep].to_csv(path_or_buf=PATH+'%s_Balances.csv' % rep.replace(' ', '_'), 
-			                                        columns=df.columns, 
-			                                        index=False)
+            # Split data by Sales Rep and write to csvs
+            logger.debug('Writing updated %s to Sales Rep csvs' % filename)
+            sales_reps = df['Salesperson'].unique()
+            for rep in sales_reps:
+                df[df['Salesperson'] == rep].to_csv(path_or_buf=PATH+'%s_Balances.csv' % rep.replace(' ', '_'), 
+                                                    columns=df.columns, 
+                                                    index=False)
 
-			return
+            return
 
 
 except Exception as e:
-	logger.error(e, exc_info=True)
+    logger.error(e, exc_info=True)
 
 
 if __name__ == '__main__':
-	reformat = DataReformat()
+    reformat = DataReformat()
 
-	PATH = '/PATH/TO/FILES/'
-	filename = 'FILENAME'
-	new_filename = 'NEW_FILENAME'
+    PATH = '/PATH/TO/FILES/'
+    filename = 'FILENAME'
+    new_filename = 'NEW_FILENAME'
 
-	errors = reformat.datareformat(PATH, filename, new_filename)
-	print errors
+    errors = reformat.datareformat(PATH, filename, new_filename)
+    print errors
 
